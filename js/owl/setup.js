84891f853011293086130eebe7ee27fa69da57e3
gleidsondevC <gleidsonlive01@gmail.com>
2021-05-01 01:02:50 -0300

    modificacoes no style

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -16,3 +16,22 @@ $('.owl-carousel').owlCarousel({
     }
   }
 })
+
+$('.owl-carousel').owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  autoplay: false,
+  autoplayTimeout: 2000,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 1
+    },
+    1000: {
+      items: 1
+    }
+  }
+})
