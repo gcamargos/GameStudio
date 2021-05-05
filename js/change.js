aec5a30975d6ff9f64e8da15245bb0c3fd2d7b03
gleidsondevC <gleidsonlive01@gmail.com>
2021-05-05 20:08:37 -0300

    teste erro img

--- js/change.js
+++ js/change.js
@@ -6,7 +6,7 @@ function changeLol() {
   let paragraph1 = 'League of Legends: Wild Rift é um jogo eletrônico do gênero MOBA desenvolvido e publicado pela Riot Games para Android, iOS, e consoles ainda desconhecidos. O jogo é uma versão adaptada de seu equivalente para PC, League of Legends.'
   let paragraph2 = 'Como em League of Legends, os jogadores controlam um personagem ("campeão") com habilidades únicas e batalham contra um time de jogadores ou inteligência artificial. O objetivo de cada time é destruir o nexus (uma construção localizada na base e protegida por outras estruturas) da equipe adversária.'
   h1.innerHTML = 'WILD RIFT'
-  img.src = '../img/wild.jpg'
+  img.src = 'wild.jpg'
   txt.innerHTML = paragraph1;
   txt2.innerHTML = paragraph2;
 }
@@ -19,7 +19,7 @@ function changeVal() {
   let paragraph2 = 'O jogo conta com varios modos de jogo como "desarme da spike","disputa da spike", "mata-mata", entre varios outros. O jogo tambem conta com o modo competitivo e o modo sem classificação para jogadores casuais.  '
 
   h1.innerHTML = 'VALORANT'
-  img.src = '/../img/val.jpg'
+  img.src = 'val.jpg'
   txt.innerHTML = paragraph1;
   txt2.innerHTML = paragraph2;
 }
@@ -48,7 +48,7 @@ function impact() {
   let paragraph2 = 'O jogo é apenas online e apresenta um modo multiplayer limitado que permite até quatro jogadores jogarem juntos. Foi lançado para Microsoft Windows, PlayStation 4, Android e iOS em 28 de setembro de 2020.'
 
   h1.innerHTML = 'GESHIN IMPACT'
-  img.src = '/../../img/impact.jpg'
+  img.src = 'impact.jpg'
 
   txt.innerHTML = paragraph1;
   txt2.innerHTML = paragraph2;
