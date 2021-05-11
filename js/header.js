8563a0529c562bf206a5ed7cd4f6f08fac46a27c
gleidsondevC <gleidsonlive01@gmail.com>
2021-05-11 17:08:23 -0300

    terminando primeira etapa, footer, entre outros

--- js/header.js
+++ js/header.js
@@ -1,5 +1,7 @@
 window.addEventListener('scroll', function () {
   let header = document.querySelector('header')
+  let btn = document.querySelector('.toTop')
   let windowscrol = window.scrollY > 500; /*quando o window tiver rolado mais de 500 pixel no eixo y */
   header.classList.toggle('scrolling-active', windowscrol)
+  btn.classList.toggle('btn-active', windowscrol)
 })
