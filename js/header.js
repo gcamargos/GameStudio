6dd8e27fe75ab7ad264c650fb338013e24285914
gleidsondevC <gleidsonlive01@gmail.com>
2021-05-13 00:47:55 -0300

    feitas animacoes

--- js/header.js
+++ js/header.js
@@ -5,3 +5,15 @@ window.addEventListener('scroll', function () {
   header.classList.toggle('scrolling-active', windowscrol)
   btn.classList.toggle('btn-active', windowscrol)
 })
+
+window.addEventListener('scroll', function () {
+
+  let animation2 = document.querySelector('#anim1')
+  let animation1 = document.querySelector('#bt')
+
+  let windowscrol = window.scrollY < 500; /*quando o window tiver rolado mais de 500 pixel no eixo y */
+  animation2.classList.toggle('anim', windowscrol)
+  animation1.classList.toggle('anim2', windowscrol)
+
+})
+
